ab7575092df42c78a4e6c00c6e1762eb909768c8
Allen Lee <6207ALS@gmail.com>
2024-03-21 19:34:02 -0400

    Removed rrweb library, added rrweb CDNs
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>

--- src/CampfireSDK.js
+++ src/CampfireSDK.js
@@ -1,13 +1,49 @@
-import * as rrweb from "rrweb";
-
 class CampfireSDK {
 	constructor() {
 
 	}
 
+	static addrrwebCDNLinks() {
+		const RRWEB_STYLESHEET_URL = "https://cdn.jsdelivr.net/npm/rrweb@latest/dist/rrweb.min.css";
+		const RRWEB_CDN_URL = "https://cdn.jsdelivr.net/npm/rrweb@latest/dist/rrweb.min.js"
+
+		const RRWEB_STYLESHEET_ELEMENT = document.createElement("LINK");
+		RRWEB_STYLESHEET_ELEMENT.setAttribute("rel", "stylesheet");
+		RRWEB_STYLESHEET_ELEMENT.setAttribute("href", RRWEB_STYLESHEET_URL);
+
+		const RRWEB_CDN_ELEMENT = document.createElement("SCRIPT");
+		RRWEB_CDN_ELEMENT.setAttribute("src", RRWEB_CDN_URL);
+
+		document.head.appendChild(RRWEB_STYLESHEET_ELEMENT)
+		document.head.appendChild(RRWEB_CDN_ELEMENT)
+	}
+
+	static addrrwebPlayerCDNLinks() {
+		const RRWEB_PLAYER_STYLESHEET_URL = "https://cdn.jsdelivr.net/npm/rrweb-player@latest/dist/style.css"
+		const RRWEB_PLAYER_CDN_URL = "https://cdn.jsdelivr.net/npm/rrweb-player@latest/dist/index.js";
+
+		const RRWEB_PLAYER_STYLESHEET_ELEMENT = document.createElement("LINK");
+		RRWEB_PLAYER_STYLESHEET_ELEMENT.setAttribute("rel", "stylesheet");
+		RRWEB_PLAYER_STYLESHEET_ELEMENT.setAttribute("href", RRWEB_PLAYER_STYLESHEET_URL);
+
+		const RRWEB_PLAYER_CDN_ELEMENT = document.createElement("SCRIPT");
+		RRWEB_PLAYER_CDN_ELEMENT.setAttribute("src", RRWEB_PLAYER_CDN_URL);
+
+		
+		document.head.appendChild(RRWEB_PLAYER_STYLESHEET_ELEMENT)
+		document.head.appendChild(RRWEB_PLAYER_CDN_ELEMENT)
+	}
+
+	static addCDNLinks() {
+		this.addrrwebCDNLinks();
+		this.addrrwebPlayerCDNLinks();
+	}
+
 	static init() {
+		this.addCDNLinks();
 		if (window) {
 			window.addEventListener("message", (e) => {
+				console.log(e.data)
 				// We want to retrieve the domain of the parent app's URL
 				// Eventually, we'll want to check if the origin is https://feedback-interface.DOMAIN
 				// for a layer of security.
