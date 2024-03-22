4c740a1a72d466f447d1d3ab14d8323a9a3f002e
Allen Lee <6207ALS@gmail.com>
2024-03-22 13:04:30 -0400

    Local URLs + CSS changes
    Co-authored-by: Weston Ludeke <westonludeke@gmail.com>
    Co-authored-by: Rachele Lang <rachelelang7672@gmail.com>
    Co-authored-by: Tess Lockey <84677774+treskey@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useState, useEffect } from "react";
-import campfireLogo from "/campfire.png";
+import { useState, useEffect, useRef } from "react";
+import campfireLogo from "/campfire-no-bg.png";
+import campfireTitle from "/campfire-title.png";
 import "./App.css";
 import CampfireSDK from "./CampfireSDK";
 
+const gradients = [
+	"redOrange", "bluePurple", "yellowBlue", "dark"
+]
+
 function App() {
+	const [index, setIndex] = useState(0)
+
 	useEffect(() => {
 		CampfireSDK.init();
 	}, [])
 
+	const changeBackground = () => {
+		setIndex(prevState => (prevState + 1) % gradients.length);
+		const gradient = gradients[index];
+
+		document.body.setAttribute("class", gradient);
+	}
+
   return (
     <>
-      <div>
-        <a href="https://github.com/campfire-previews/campfire-app" target="_blank">
-          <img src={campfireLogo} className="logo" alt="campfire logo" />
-        </a>
+      <div id="app">
+        <img onClick={changeBackground} src={campfireLogo} className="logo" alt="campfire logo" />
         <h1>
           open-source self-hosted collaborative deploy previews
         </h1>
--- src/CampfireSDK.js
+++ src/CampfireSDK.js
@@ -1,46 +1,11 @@
+import * as rrweb from "rrweb";
+
 class CampfireSDK {
 	constructor() {
 
 	}
 
-	static addrrwebCDNLinks() {
-		const RRWEB_STYLESHEET_URL = "https://cdn.jsdelivr.net/npm/rrweb@latest/dist/rrweb.min.css";
-		const RRWEB_CDN_URL = "https://cdn.jsdelivr.net/npm/rrweb@latest/dist/rrweb.min.js"
-
-		const RRWEB_STYLESHEET_ELEMENT = document.createElement("LINK");
-		RRWEB_STYLESHEET_ELEMENT.setAttribute("rel", "stylesheet");
-		RRWEB_STYLESHEET_ELEMENT.setAttribute("href", RRWEB_STYLESHEET_URL);
-
-		const RRWEB_CDN_ELEMENT = document.createElement("SCRIPT");
-		RRWEB_CDN_ELEMENT.setAttribute("src", RRWEB_CDN_URL);
-
-		document.head.appendChild(RRWEB_STYLESHEET_ELEMENT)
-		document.head.appendChild(RRWEB_CDN_ELEMENT)
-	}
-
-	static addrrwebPlayerCDNLinks() {
-		const RRWEB_PLAYER_STYLESHEET_URL = "https://cdn.jsdelivr.net/npm/rrweb-player@latest/dist/style.css"
-		const RRWEB_PLAYER_CDN_URL = "https://cdn.jsdelivr.net/npm/rrweb-player@latest/dist/index.js";
-
-		const RRWEB_PLAYER_STYLESHEET_ELEMENT = document.createElement("LINK");
-		RRWEB_PLAYER_STYLESHEET_ELEMENT.setAttribute("rel", "stylesheet");
-		RRWEB_PLAYER_STYLESHEET_ELEMENT.setAttribute("href", RRWEB_PLAYER_STYLESHEET_URL);
-
-		const RRWEB_PLAYER_CDN_ELEMENT = document.createElement("SCRIPT");
-		RRWEB_PLAYER_CDN_ELEMENT.setAttribute("src", RRWEB_PLAYER_CDN_URL);
-
-		
-		document.head.appendChild(RRWEB_PLAYER_STYLESHEET_ELEMENT)
-		document.head.appendChild(RRWEB_PLAYER_CDN_ELEMENT)
-	}
-
-	static addCDNLinks() {
-		this.addrrwebCDNLinks();
-		this.addrrwebPlayerCDNLinks();
-	}
-
 	static init() {
-		this.addCDNLinks();
 		if (window) {
 			window.addEventListener("message", (e) => {
 				console.log(e.data)
@@ -48,6 +13,7 @@ class CampfireSDK {
 				// Eventually, we'll want to check if the origin is https://feedback-interface.DOMAIN
 				// for a layer of security.
 				if (e.origin === "http://localhost:5173") {
+					console.log("origin: ", e.origin)
 					new rrweb.record({
 						emit() {},
 						recordCrossOriginIframes: true,
